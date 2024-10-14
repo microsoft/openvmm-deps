22066edb720a9c1a9e1e18b1078d20eb91aac22d
Steven Malis <137308034+smalis-msft@users.noreply.github.com>
2024-10-14 11:26:05 -0400

    Shrink openssl by removing unwinding info

--- pkg/openssl3/build.sh
+++ pkg/openssl3/build.sh
@@ -2,6 +2,6 @@
 set -e
 set -x
 
-"$SRCDIR/Configure" "linux-$ARCH" no-shared --cross-compile-prefix="$ARCH-linux-musl-" --prefix="$SYSROOT"
+"$SRCDIR/Configure" "linux-$ARCH" no-shared --cross-compile-prefix="$ARCH-linux-musl-" --prefix="$SYSROOT" -fno-asynchronous-unwind-tables
 make -j`nproc`
 make -j`nproc` install_sw
