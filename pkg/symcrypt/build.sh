3ce56b876ec332ddfe634921b0aca0e50a7bb564
Steven Malis <stevenmalis@microsoft.com>
2026-04-24 11:09:57 -0400

    done debugging

--- pkg/symcrypt/build.sh
+++ pkg/symcrypt/build.sh
@@ -4,7 +4,7 @@ set -e
 
 cd $SRCDIR
 # Specify git metadata manually to avoid relying on the .git directory being present
-SYMCRYPT_BRANCH="main" SYMCRYPT_COMMIT_HASH="748c20f1fc486beca1a2679ed06492712cfdc950" SYMCRYPT_COMMIT_TIMESTAMP="2026-03-28T19:22:38-04:00" scripts/build.py cmake --config Release --no-strip-binary out
+SYMCRYPT_BRANCH="main" SYMCRYPT_COMMIT_HASH="748c20f1fc486beca1a2679ed06492712cfdc950" SYMCRYPT_COMMIT_TIMESTAMP="2026-03-28T19:22:38-04:00" scripts/build.py cmake --config Release out
 cp out/module/generic/*.so* $SYSROOT/lib/
 cp out/lib/*.a $SYSROOT/lib/
 cp out/inc/*.h $SYSROOT/include/
