64c65256e9e57d835dfd79f8cee106e9883c78ce
Steven Malis <stevenmalis@microsoft.com>
2026-06-04 12:54:04 -0400

    Delete the "sample"

--- sample/build.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    println!("cargo:rustc-link-search=../sysroot/usr/lib");
-    println!("cargo:rustc-link-lib=static=sample");
-    println!("cargo:rustc-link-lib=static=stdc++");
-}
--- sample/src/main.rs
+++
@@ -1,9 +0,0 @@
-use std::ffi::c_char;
-
-extern "C" {
-    fn sample_entrypoint(x: *const c_char);
-}
-
-fn main() {
-    unsafe { sample_entrypoint(b"hello\0".as_ptr().cast()) }
-}
