398d465882b9b5148ba1eb207fc20789a44a643d
Steven Malis <stevenmalis@microsoft.com>
2026-06-11 17:21:41 -0400

    Pin new supply chains

--- pkg/Tools/deps.sh
+++ pkg/Tools/deps.sh
@@ -39,7 +39,30 @@ util-linux
 
 tdnf install -y $packages
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- \
-    -y --no-modify-path --default-toolchain stable --profile minimal \
+RUSTUP_VERSION="1.29.0"
+RUST_TOOLCHAIN="1.96.0"
+
+case "$(uname -m)" in
+    x86_64)
+        rustup_arch="x86_64-unknown-linux-musl"
+        rustup_init_sha256="9cd3fda5fd293890e36ab271af6a786ee22084b5f6c2b83fd8323cec6f0992c1"
+        ;;
+    aarch64)
+        rustup_arch="aarch64-unknown-linux-musl"
+        rustup_init_sha256="88761caacddb92cd79b0b1f939f3990ba1997d701a38b3e8dd6746a562f2a759"
+        ;;
+    *)
+        echo "unsupported host architecture: $(uname -m)" >&2
+        exit 1
+        ;;
+esac
+
+curl --proto '=https' --tlsv1.2 -sSfo /tmp/rustup-init \
+    "https://static.rust-lang.org/rustup/archive/${RUSTUP_VERSION}/${rustup_arch}/rustup-init"
+echo "${rustup_init_sha256}  /tmp/rustup-init" | sha256sum -c -
+chmod +x /tmp/rustup-init
+/tmp/rustup-init \
+    -y --no-modify-path --default-toolchain "$RUST_TOOLCHAIN" --profile minimal \
     --target x86_64-unknown-linux-musl \
     --target aarch64-unknown-linux-musl
+rm /tmp/rustup-init
